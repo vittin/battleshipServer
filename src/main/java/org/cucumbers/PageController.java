7a06b0798e670fdbfed4cb5664c502adacc360ef
vittin <97mati@wp.pl>
2016-06-10 21:10:06 +0200

    initial commit

---
+++ src/main/java/org/cucumbers/PageController.java
@@ -0,0 +1,24 @@
+package org.cucumbers;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/** Created by Mateusz on 2016-06-10. */
+
+@EnableAutoConfiguration
+@Controller
+@CrossOrigin(origins = "http://localhost:63342")
+@RequestMapping("")
+public class PageController {
+    @RequestMapping(value = "/initGame", method = RequestMethod.GET)
+    @ResponseBody
+    public void startGame(){
+        System.out.println("hello");
+    }
+
+
+}
